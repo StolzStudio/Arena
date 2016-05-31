2318250c466f4b990c6f35c1c983b71e750d5b5a
Nicholas Glushchenko <ruscoolman@gmail.com>
2016-05-31 13:43:21 +1000

    bild game frame

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 
 #include <iostream>
-
+#include "map.hpp"
 
 int main(int argc, const char * argv[]) {
+    int q = -1;
     
+    Map arena = *new Map(20);
+    
+    initscr();
+    noecho();
+    erase();
+    
+    arena.drawMap();
+    q = getch();
     return 0;
 }
